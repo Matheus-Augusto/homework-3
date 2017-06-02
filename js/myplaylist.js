026fab552d5f6a0adb3b0cc32b19eba9defd5fb3
Matheus-Augusto <matheusanp156@gmail.com>
2017-06-02 14:34:11 -0300

    Playlist - Java

---
+++ js/myplaylist.js
@@ -0,0 +1,62 @@
+/**
+ * Created by 23rd and Walnut
+ * www.23andwalnut.com
+ * User: Saleem El-Amin
+ * Date: 6/8/11
+ * Time: 9:39 AM
+ */
+
+var myPlaylist = [
+
+    {
+        mp3:'mix/01.mp3',
+        title:'Locked Out of Heaven',
+        artist:'Bruno Mars',
+        rating:5,
+        buy:'#',
+        price:'0.99',
+        duration:'03:53',
+        cover:'mix/01-capa.jpg'
+    },
+
+    {
+        mp3:'mix/02.mp3',
+        title:'Sugar',
+        artist:'Maroon 5',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'03:55',
+        cover:'mix/02-capa.jpg'
+    },
+    {
+        mp3:'mix/03.mp3',
+        title:'Heathens',
+        artist:'Twenty One Pilots',
+        rating:5,
+        buy:'#',
+        price:'0.99',
+        duration:'03:16',
+        cover:'mix/03-capa.jpg'
+    },
+    {
+        mp3:'mix/04.mp3',
+        title:'The Heart Wants What It Wants',
+        artist:'Selena Gomez',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'03:44',
+        cover:'mix/04-capa.png'
+    },
+    {
+        mp3:'mix/05.mp3',
+        title:'Thinking Out Loud',
+        artist:'Ed Sheeran',
+        rating:5,
+        buy:'#',
+        price:'0.99',
+        duration:'04:41',
+        cover:'mix/05-capa.jpg'
+    },
+];
